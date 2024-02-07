70a761e3d7a63f9236afe65c941fe32cfda8b1be
Vatsal Gosai <68988968+Vatsal-Gosai@users.noreply.github.com>
2024-02-07 13:58:38 +0530

    Add files via upload

---
+++ tic-tc-toe/src/TicTacToe/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Square(props) {
+  return (
+    <div onClick={props.onClick} className="square">
+      <h5>{props.value}</h5>
+    </div>
+  );
+}
+
+export default Square;
